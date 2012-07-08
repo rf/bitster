2f6f1bf059bd3e10f7eceb75442426b304f1f876
Martin Miralles-Cordal <axalon@eden.rutgers.edu>
2012-07-08 14:50:50 -0400

    Deputy now parses tracker response properly and forwards peer list to manager and announces every [interval] seconds. Added sender field to Memo.

--- src/libbitster/Deputy.java
+++ src/libbitster/Deputy.java
@@ -1,12 +1,14 @@
 package libbitster;
 
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
-import java.net.ServerSocket;
 import java.net.URL;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -17,56 +19,81 @@ import java.util.Random;
  */
 public class Deputy extends Actor {
   
+  private String state; // states:
+  // 'init': just created, waiting to establish a connection
+  // 'error': error occured, exception property will be populated
+  // 'normal': operating normally (may add more such states later)
+  
   private String announceURL;
   private String infoHash;
   private byte[] peerID;
-  private TorrentInfo metainfo;
+  //private TorrentInfo metainfo;
+  private int listenPort;
+  private int announceInterval;
+  private Manager manager;
+  Calendar lastAnnounce;
   
-  //Listens for incoming peer connections
-  private ServerSocket listen;
+  public Exception exception;         // set to an exception if one occurs
+
+  /**
+   * Constructs a Deputy object
+   * @param metainfo The data from the metainfo file
+   * @param port The port the manager is listening for incoming connections on
+   */
+  public Deputy(TorrentInfo metainfo, int port, Manager manager)
+  {
+      //this.metainfo = metainfo;
+      this.listenPort = port;
+      this.manager = manager;
+      
+      // assemble our announce URL from metainfo
+      announceURL = metainfo.announce_url.getProtocol() + "://" +
+        metainfo.announce_url.getHost() + ":" + metainfo.announce_url.getPort()
+        + metainfo.announce_url.getPath();
+      
+      // encode our info hash
+      ByteBuffer rawInfoHash = metainfo.info_hash;
+      StringBuffer infoHashSB = new StringBuffer();
+      while(rawInfoHash.hasRemaining())
+      {
+        infoHashSB.append("%");
+        String hexEncode = Integer.toHexString(0xFF & rawInfoHash.get());
+        if(hexEncode.length() == 1)
+          infoHashSB.append("0");
+        infoHashSB.append(hexEncode);
+      }
+      infoHash = infoHashSB.toString();
+      
+      // generate peer ID if we haven't already
+      if(peerID == null)
+        peerID = generatePeerID();
+      
+      this.state = "init";
+      
+      // we're done setting up variables, now connect
+      announce();
+  }
 
   @Override
   protected void receive (Memo memo)
   {
-    if(memo.getType().equals("info"))
+    if(memo.getType().equals("list"))
     {
-      try
-      {
-        metainfo = (TorrentInfo) memo.getPayload();
-        
-        // assemble our announce URL from metainfo
-        announceURL = metainfo.announce_url.getProtocol() + "://" +
-          metainfo.announce_url.getHost() + ":" + metainfo.announce_url.getPort()
-          + metainfo.announce_url.getPath();
-      
-        // listen for connections
-        this.listen = new ServerSocket(6881);
-        
-        // encode our info hash
-        ByteBuffer rawInfoHash = metainfo.info_hash;
-        StringBuffer infoHashSB = new StringBuffer();
-        while(rawInfoHash.hasRemaining())
-        {
-          infoHashSB.append("%");
-          String hexEncode = Integer.toHexString(0xFF & rawInfoHash.get());
-          if(hexEncode.length() == 1)
-            infoHashSB.append("0");
-          infoHashSB.append(hexEncode);
-        }
-        infoHash = infoHashSB.toString();
-        
-        // generate peer ID if we haven't already
-        if(peerID == null)
-          peerID = generatePeerID();
-        
-        // we're done setting up variables, now connect
-        contactTracker();
-        
-      } catch (IOException e) {
-        // TODO Auto-generated catch block
-        e.printStackTrace();
-        return;
-      }
+        announce(); // get updated peer list and send it to manager
+    }
+  }
+  
+  /**
+   * Announce at regular intervals
+   */
+  @Override
+  protected void idle () {
+    try { sleep(1000); } catch (Exception e) {}
+    
+    if(Calendar.getInstance().getTimeInMillis() - this.lastAnnounce.getTimeInMillis()
+        > 1000*this.announceInterval)
+    {
+      announce();
     }
   }
   
@@ -95,12 +122,17 @@ public class Deputy extends Actor {
   /**
    * Sends an HTTP GET request and gets fresh info from the tracker.
    */
-  private void contactTracker()
+  @SuppressWarnings("unchecked")
+  private void announce()
   {
     if(announceURL == null)
       return;
     else
     {
+      // reset our timer
+      this.lastAnnounce = Calendar.getInstance();
+      System.out.println("Announcing...");
+      
       StringBuffer finalURL = new StringBuffer();
       // add announce URL
       finalURL.append(announceURL);
@@ -114,34 +146,96 @@ public class Deputy extends Actor {
       finalURL.append(new String(peerID));
       
       // add port
-      finalURL.append("&port=6881"); // TODO: don't hardcode this
+      finalURL.append("&port=");
+      finalURL.append(this.listenPort);
       
-      // add rest
-      // TODO: split this all up and figure it out programmatically
-      finalURL.append("&uploaded=0&downloaded=0");
+      /* TODO: Change up/down/left gathering into a memo? */
+      // add uploaded
+      finalURL.append("&uploaded=");
+      finalURL.append(manager.getUploaded());
+      
+      // add downloaded
+      finalURL.append("&downloaded=");
+      finalURL.append(manager.getDownloaded());
       
       // add amount left
-      // TODO: do this right
       finalURL.append("&left=");
-      finalURL.append(metainfo.file_length);
+      finalURL.append(manager.getLeft());
       
-      System.out.println(finalURL.toString());
       try {
+        // send request to tracker
         URL tracker = new URL(finalURL.toString());
-        /* temporary crappy placeholder code */
+        
+        // read response
         InputStream is = tracker.openStream();
         byte[] bytes = new byte[is.available()];
         is.read(bytes);
-        System.out.println(new String(bytes));
-        /* end temporary crappy placeholder code */
+        
+        // bdecode response
+        @SuppressWarnings("rawtypes")
+        Map response = (Map) Bencoder2.decode(bytes);
+        
+        // get our peer list and work it into something nicer
+        @SuppressWarnings("rawtypes")
+        ArrayList<Map> rawPeers =
+            (ArrayList<Map>) response.get(ByteBuffer.wrap("peers".getBytes()));
+        ArrayList<Map<String,String>> peers = parsePeers(rawPeers);
+        
+        // send updated peer list to manager
+        manager.post(new Memo("peers", peers, this));
+        
+        // get our announce interval
+        announceInterval = (int) response.get(ByteBuffer.wrap("interval".getBytes()));
+        
+        this.state = "normal";
+               
       } catch (MalformedURLException e) {
-        // TODO Auto-generated catch block
+        this.exception = e;
+        this.state = "error";
         e.printStackTrace();
       } catch (IOException e) {
-        // TODO Auto-generated catch block
+        this.exception = e;
+        this.state = "error";
+        e.printStackTrace();
+      } catch (BencodingException e) {
+        this.exception = e;
+        this.state = "error";
         e.printStackTrace();
       }
     }
+  }
+  
+  /**
+   * Takes the raw peer list from the tracker response and processes it into something
+   * that's nicer to work with
+   * @param rawPeerList The {@code ArrayList<{@code Map}>} of peers sent from announce()
+   * @return An ArrayList<Map<String, String>> of peers and their information
+   */
+  private ArrayList<Map<String, String>> parsePeers(@SuppressWarnings("rawtypes") ArrayList<Map> rawPeerList)
+  {
+    ArrayList<Map<String, String>> processedPeerList = new ArrayList<Map<String, String>>();
+    for(int i = 0; i < rawPeerList.size(); ++i)
+    {
+      HashMap<String,String> peerInfo = new HashMap<String,String>();
+      
+      // get this peer's peer ID
+      ByteBuffer peer_id_bytes = (ByteBuffer) rawPeerList.get(i).get(ByteBuffer.wrap("peer id".getBytes()));
+      String peer_id = new String(peer_id_bytes.array());
+      peerInfo.put("peer id", peer_id);
+      
+      // get this peer's ip
+      ByteBuffer ip_bytes = (ByteBuffer) rawPeerList.get(i).get(ByteBuffer.wrap("ip".getBytes()));
+      String ip = new String(ip_bytes.array());
+      peerInfo.put("ip", ip);
+      
+      // get this peer's port
+      Integer port = (Integer) rawPeerList.get(i).get(ByteBuffer.wrap("port".getBytes()));
+      peerInfo.put("port", port.toString());
+      
+      // add it to our peer list
+      processedPeerList.add(peerInfo);
+    }
+    return processedPeerList;
   }
   
 }
