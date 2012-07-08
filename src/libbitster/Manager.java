1243d100ca418171b50d2845f41f094a2310fb1f
Martin Miralles-Cordal <axalon@eden.rutgers.edu>
2012-07-07 21:40:06 -0400

    Deputy now receives metainfo rather than announce URL, due to it needing things such as the info hash from it, and had it just get the whole thing rather than define a bunch of separate memos that are essentially one transaction.

--- src/libbitster/Manager.java
+++ src/libbitster/Manager.java
@@ -1,16 +1,45 @@
 package libbitster;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Manager extends Actor {
   
+  // the contents of the metainfo file
+  @SuppressWarnings("unused")
   private TorrentInfo metainfo;
+  
+  // communicates with tracker
   private Deputy deputy;
   
+  // Listens for incoming peer connections
+  private ServerSocket listen;
+  
+  /**
+   * Instantiates the Manager and its Deputy, sending a memo containing the 
+   * tracker's announce URL.
+   * @param metainfo A {@link TorrentInfo} object containing information about
+   * a torrent.
+   */
   public Manager(TorrentInfo metainfo)
   {
     super();
     this.metainfo = metainfo;
-    deputy = new Deputy();
-    deputy.post(new Memo("url", metainfo.announce_url));
+    try {
+      this.listen = new ServerSocket(6881);
+      
+      deputy = new Deputy();
+      deputy.post(new Memo("info", metainfo));
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }    
+  }
+  
+  protected void receive (Memo memo) {
+    
+    // TODO: get messages
+    return;
   }
  
 }
