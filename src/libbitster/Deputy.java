7ac3bb3a31abf84f2efb0da0a947890452ee8633
Martin Miralles-Cordal <axalon@eden.rutgers.edu>
2012-07-07 22:44:37 -0400

    Successfully connects to tracker. Code is still a hacky mess

--- src/libbitster/Deputy.java
+++ src/libbitster/Deputy.java
@@ -1,5 +1,10 @@
 package libbitster;
 
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.nio.ByteBuffer;
 import java.util.Random;
 
@@ -12,17 +17,31 @@ import java.util.Random;
 public class Deputy extends Actor {
   
   private String announceURL;
-  private ByteBuffer infoHash;
+  private String infoHash;
   private byte[] peerID;
+  private TorrentInfo metainfo;
 
   @Override
   protected void receive (Memo memo)
   {
     if(memo.getType().equals("info"))
     {
-      TorrentInfo metainfo = (TorrentInfo) memo.getPayload();
-      announceURL = metainfo.announce_url.getPath();
-      infoHash = metainfo.info_hash;
+      metainfo = (TorrentInfo) memo.getPayload();
+      announceURL = metainfo.announce_url.getProtocol() + "://" +
+          metainfo.announce_url.getHost() + ":" + metainfo.announce_url.getPort()
+          + metainfo.announce_url.getPath();
+      
+      ByteBuffer rawInfoHash = metainfo.info_hash;
+      StringBuffer infoHashSB = new StringBuffer();
+      while(rawInfoHash.hasRemaining())
+      {
+        infoHashSB.append("%");
+        String hexEncode = Integer.toHexString(0xFF & rawInfoHash.get());
+        if(hexEncode.length() == 1)
+          infoHashSB.append("0");
+        infoHashSB.append(hexEncode);
+      }
+      infoHash = infoHashSB.toString();
       
       if(peerID == null)
         peerID = generatePeerID();
@@ -41,14 +60,13 @@ public class Deputy extends Actor {
     byte[] id = new byte[20];
     // generating random peer ID. BITS + 16 digits = 20 characters
     Random r = new Random(System.currentTimeMillis());
-    peerID = new byte[20];
-    peerID[0] = 'B';
-    peerID[1] = 'I';
-    peerID[2] = 'T';
-    peerID[3] = 'S';
+    id[0] = 'B';
+    id[1] = 'I';
+    id[2] = 'T';
+    id[3] = 'S';
     for(int i = 4; i < 20; i++)
     {
-      peerID[i] = (byte) ('A' +  r.nextInt(26));
+      id[i] = (byte) ('A' +  r.nextInt(26));
     }
     
     return id;
@@ -63,7 +81,46 @@ public class Deputy extends Actor {
       return;
     else
     {
+      StringBuffer finalURL = new StringBuffer();
+      // add announce URL
+      finalURL.append(announceURL);
       
+      // add info hash
+      finalURL.append("?info_hash=");
+      finalURL.append(infoHash.toString());
+      
+      // add peer ID
+      finalURL.append("&peer_id=");
+      String s = "";
+      finalURL.append(new String(peerID));
+      
+      // add port
+      finalURL.append("&port=6881"); // TODO: don't hardcode this
+      
+      // add rest
+      // TODO: split this all up and figure it out programmatically
+      finalURL.append("&uploaded=0&downloaded=0");
+      
+      // add amount left
+      // TODO: do this right
+      finalURL.append("&left=");
+      finalURL.append(metainfo.file_length);
+      
+      System.out.println(finalURL.toString());
+      try {
+        URL tracker = new URL(finalURL.toString());
+        InputStream is = tracker.openStream();
+        byte b;
+        byte[] bytes = new byte[is.available()];
+        is.read(bytes);
+        System.out.println(new String(bytes));
+      } catch (MalformedURLException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+      }
     }
   }
   
