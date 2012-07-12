1f3acb6932833c0203f6d0ad87c7ec8caf4cdafc
Russell Frank <russ.frank@rutgers.edu>
2012-07-11 23:46:24 -0400

    Adding some comments to Timeout

--- src/libbitster/Timeout.java
+++ src/libbitster/Timeout.java
@@ -3,6 +3,9 @@ package libbitster;
 import java.util.*;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+// A class for scheduling memos to be delivered back to the sender after a 
+// specified interval. Singleton.
+
 public class Timeout extends Actor {
   private int last = 0;
 
@@ -22,6 +25,9 @@ public class Timeout extends Actor {
 
     if (last != 0) {
       int elapsed = curr - last;
+
+      // Loop over each timeout we know about and reduce its time by the 
+      // elapsed time.  When it's up, post the memo back to the sender.
 
       Iterator<TimeoutInfo> i = timeouts.iterator();
       while (i.hasNext()) {
@@ -44,6 +50,8 @@ public class Timeout extends Actor {
     timeouts.add(info);
   }
 
+  // ## set
+  // Set a timeout.  Takes a timeout and memo as arguments.
   public static synchronized void set (int timeout, Memo memo) {
     if (instance == null) instance = new Timeout();
     instance.setTimeout(timeout, memo);
