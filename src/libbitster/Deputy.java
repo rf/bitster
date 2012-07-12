93a6357492a813df2a49e04f830c51a4c41826ae
Martin Miralles-Cordal <axalon@eden.rutgers.edu>
2012-07-11 22:59:06 -0400

    Fixed a warning in Deputy

--- src/libbitster/Deputy.java
+++ src/libbitster/Deputy.java
@@ -1,7 +1,6 @@
 package libbitster;
 
 import java.io.DataInputStream;
-import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
