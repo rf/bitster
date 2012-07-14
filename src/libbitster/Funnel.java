53fd0e70fdab0c7cb6b286b80b032392dd8eeba8
Bill <surgent@eden.rutgers.edu>
2012-07-13 23:36:52 -0400

    Removed unnecessary block of code in Piece that throws the same exception that is caught XD and changed isValid() to take the expected hash rather than storing it with the piece. Progress on funnel methods.

--- src/libbitster/Funnel.java
+++ src/libbitster/Funnel.java
@@ -1,7 +1,37 @@
 package libbitster;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Funnel extends Actor {
-  protected void receive (Memo memo) {
+  private int size;
+  private int pieceSize;
+  private List<Piece> pieces;
+  
+  public Funnel(int size, int pieceSize) {
+    if(size < 0 || pieceSize < 0 || size < pieceSize)
+      throw new IllegalArgumentException();
     
+    this.size = size;
+    this.pieceSize = pieceSize;
+    int numPieces = (int)Math.ceil((double)size / (double)pieceSize);
+    pieces = Collections.synchronizedList(new ArrayList<Piece>(numPieces));
+  }
+  
+  protected void receive (Memo memo) {
+    if(!(memo.getPayload() instanceof Piece))
+      throw new IllegalArgumentException("Funnel expects a Piece");
+    
+    Piece piece = (Piece)memo.getPayload();
+    
+    if(piece.getNumber() < pieces.size() - 1 && piece.getData().length != pieceSize)
+      throw new IllegalArgumentException("Piece " + piece.getNumber() + " is the wrong size");
+    //This is a little fancy around the part with the modulus operator
+    //Basically it just gets the minimum number of bytes that the last piece should contain
+    if(piece.getNumber() == pieces.size() - 1 && piece.getData().length < ((size - 1) % pieceSize) + 1)
+      throw new IllegalArgumentException("Piece " + piece.getNumber() + " is too small");
+    
+    pieces.set(piece.getNumber(), piece);
   }
 }
