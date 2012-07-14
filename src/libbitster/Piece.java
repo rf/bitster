a8b6d0bc2bea7d4c3f6cbff3904935684200687d
Bill <surgent@eden.rutgers.edu>
2012-07-13 20:00:04 -0400

    Finished current set of methods for Piece and fixed some bugs

--- src/libbitster/Piece.java
+++ src/libbitster/Piece.java
@@ -7,9 +7,12 @@ public class Piece {
   private int number;
   private int blockSize;
   private byte[] data;
-  private BitSet compleated;
+  private BitSet completed;
   
   public Piece(int number, int blockSize, int size) {
+    //Sanity checks
+    if(number < 0 || blockSize <= 0 || size <= 0)
+      throw new IllegalArgumentException("Arguments must be > 0 (except number which may = 0)");
     if(blockSize > size)
       throw new IllegalArgumentException("blockSize must be < size");
     
@@ -23,7 +26,8 @@ public class Piece {
       throw new OutOfMemoryError();
     }
     
-    compleated = new BitSet( (int)Math.ceil((double)size / (double)blockSize) );
+    //One bit for each block
+    completed = new BitSet( (int)Math.ceil((double)size / (double)blockSize) );
   }
   
   public void addBlock(int begin, ByteBuffer block) {
@@ -36,6 +40,10 @@ public class Piece {
     if(begin + block.limit() > data.length || begin < 0)
       throw new IllegalArgumentException("block under/overflows the buffer for this piece");
     
+    //If this piece was already downloaded then there is nothing to to
+    if(completed.get(begin % blockSize))
+      return;
+    
     //Check to make sure not special case where final block would be smaller than the rest
     //Also check to make sure 'block' is of length 'blockSize'
     if( (begin < (data.length / blockSize) * blockSize) && (block.limit() != blockSize) )
@@ -46,9 +54,25 @@ public class Piece {
     //Copy block over to this piece
     for(int i = begin, l = begin + block.limit(); i < l; ++i)
       data[i] = block.get();
+    
+    completed.set(begin % blockSize);
   }
   
   public boolean finished() {
-    return false; //TODO
+    int blocks = (int)Math.ceil((double)data.length / (double)blockSize);
+    
+    //If any block is not completed than the piece is not finished
+    for(int i=0; i<blocks; ++i)
+      if(!completed.get(i))
+        return false;
+    
+    return true;
+  }
+  
+  public final byte[] getData() {
+    if(!finished())
+      throw new IllegalStateException("Piece is not finished");
+      
+    return data;
   }
 }
