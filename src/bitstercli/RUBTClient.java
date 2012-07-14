56df3bb13307e9c65878ff713dd465986a5f133a
martin <martin@localhost.localdomain>
2012-07-14 19:03:02 -0400

    Added some error checking code, added target path to Manager constructor.

--- src/bitstercli/RUBTClient.java
+++ src/bitstercli/RUBTClient.java
@@ -3,7 +3,6 @@ package bitstercli;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.logging.*;
 
@@ -18,26 +17,53 @@ public class RUBTClient {
    */
   public static void main(String[] args) {
     Logger log = Logger.getLogger("bitster");
-
+    
+    // check if we have a valid number of arguments
+    if(args.length != 2)
+    {
+      log.log(Level.SEVERE, "Error: Invalid number of arguments.");
+      return;
+    }
+    
+    // validate argument 1
+    File torrentFile = new File(args[0]);
+    if(!torrentFile.exists() || torrentFile.isDirectory())
+    {
+      log.log(Level.SEVERE, "Error: " + args[0] + " is not a file.");
+      return;
+    }
+    
+    // validate argument 2
+    File dest = new File(args[1]);
+    if(dest.exists())
+      log.log(Level.SEVERE, "Error: destination file exists.");
+    else
+    {
+      try {
+          // try to create file to validate target name
+          dest.createNewFile();
+          dest.delete();
+      } catch (IOException e) {
+        log.log(Level.SEVERE, "Error: invalid destination file.");
+        return;
+      }
+    }
+    
     try {
-      File f = new File(args[0]);
-      byte[] torrentBytes = new byte[(int) f.length()]; 
+      byte[] torrentBytes = new byte[(int) torrentFile.length()]; 
       DataInputStream dis;
-      dis = new DataInputStream(new FileInputStream(f));
+      dis = new DataInputStream(new FileInputStream(torrentFile));
       dis.readFully(torrentBytes);
       dis.close();
       TorrentInfo metainfo = new TorrentInfo(torrentBytes);
-      Manager manager = new Manager(metainfo);
+      Manager manager = new Manager(metainfo, dest);
       manager.start();
-    } catch (FileNotFoundException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
     } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+      log.log(Level.SEVERE, "Error: unable to read torrent file.");
+      return;
     } catch (BencodingException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
+      log.log(Level.SEVERE, "Error: invalid or corrupt torrent file.");
+      return;
     }
   }
 
