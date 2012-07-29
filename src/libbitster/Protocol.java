e90cd5d56fa6ec5d9549e34b0ffbf330dc5423b6
Russell Frank <russ.frank@rutgers.edu>
2012-07-29 18:06:37 -0400

    Protocol: throw in getLength; handle in onReadable
    Throwing in getLength rather than handling it there with the error
    function.  This prevents a `CancelledKeyException` by making sure that
    the Communicator function onReadble returns false, so overlord doesn't
    try to interact with the selection key.

--- src/libbitster/Protocol.java
+++ src/libbitster/Protocol.java
@@ -196,16 +196,16 @@ public class Protocol implements Communicator {
 
   // ## findLength
   // Grab the length of the next message out of the read buffer if possible
-  public void findLength () {
+  public void findLength () throws Exception {
     // If we've read at least four bytes, we haven't gotten a length yet,
     // and we're not reading a handshake message, then grab the length out of
     // the readBuffer.
-    if (length == -1 && numRead >= 4 && state != "handshake") 
+    if (length == -1 && numRead >= 4 && !state.equals("handshake"))
       // add 4 to account for the length of the integer specifying the length
       length = readBuffer.getInt(0) + 4;
 
     // If we expect a handshake and we don't have a length yet,
-    else if (length == -1 && numRead >= 1 && state == "handshake") 
+    else if (length == -1 && numRead >= 1 && state.equals("handshake")) 
       // `length` here is actually just the length of the protocol identifier
       // string.  We need to add 49 to account for the rest of the message.
       length = ((int) readBuffer.get(0)) + 49;
@@ -213,7 +213,7 @@ public class Protocol implements Communicator {
     // 32000 is arbitrary max message size
     if ((length < 0 || length > 32000) && length != -1) {  
       Log.error("Got invalid message length from peer: " + length);
-      error(new Exception("invalid message length"));
+      throw new Exception("invalid message length");
     }
   }
 
