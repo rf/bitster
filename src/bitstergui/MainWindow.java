9b08d661e622ec8ea5694edbc4424c1c7d2040ac
Martin Miralles-Cordal <axalon@eden.rutgers.edu>
2012-07-31 19:52:50 -0400

    Created MainWindow class

---
+++ src/bitstergui/MainWindow.java
@@ -0,0 +1,12 @@
+package bitstergui;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+  private static final long serialVersionUID = 0xDEADBEEFL;
+  
+  public MainWindow() {
+    
+  }
+}
