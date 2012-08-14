03a232a71ca7c9084a26681e2a55c081700e942d
Martin Miralles-Cordal <axalon@eden.rutgers.edu>
2012-08-14 15:42:35 -0400

    Deleted useless import

--- src/bitstergui/Gui.java
+++ src/bitstergui/Gui.java
@@ -3,7 +3,6 @@ package bitstergui;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
 
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
